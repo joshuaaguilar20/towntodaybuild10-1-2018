36dea6e64ac3dadfea0b47e9a5749629abb6842a
Joshua-Aguilar <joshuaaguilar20@gmail.com>
2018-08-11 15:53:03 -0700

    test

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ app.use(passport.session());
 
 require('./routes/authroutes')(app);
 require('./routes/billing')(app);
-const PORT = process.env.PORT || 5000 //allows heroku to choose port. looks at underlying enviroment. 
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+ 
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+ 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT); // use my port or production port
